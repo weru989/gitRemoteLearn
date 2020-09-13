2b60c5b2d417b1d1193af3a1d044c523462004a0
alyAds <aliarmandaulay62@gmail.com>
2020-09-13 22:38:51 +0700

    menambah paragpraf pada index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
 	<h1>Hai dunia!</h1>
+	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos provident illum ullam eum cum soluta maxime fugit error tempore inventore, vero facilis. Consequuntur molestias cupiditate eveniet similique alias soluta perferendis.</p>
 </body>
 </html>

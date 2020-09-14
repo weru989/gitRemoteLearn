ad8b16715f473448b1a9a13b79c3ac4db3432f91
alyAds <aliarmandaulay62@gmail.com>
2020-09-14 22:36:17 +0700

    menambah article baru

--- index.php
+++ index.php
@@ -12,5 +12,7 @@
 	<h2>Let's try...</h2>
 	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos provident illum ullam eum cum soluta maxime fugit error tempore inventore, vero facilis. Consequuntur molestias cupiditate eveniet similique alias soluta perferendis.</p>
 	<br>
+	<h2>Trending now?</h2>
+	<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, officia ut ad odio ex modi nesciunt omnis, dignissimos. Totam perspiciatis soluta cum quaerat maiores. Error voluptas porro reprehenderit autem, nesciunt.</p>
 </body>
 </html>

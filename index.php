84e7058f1abcfe4888a5b356c222f4a9393ccf69
alyAds <71181791+alyAds@users.noreply.github.com>
2020-09-14 22:17:27 +0700

    menambah article baru

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
 	<h1>Hai dunia!</h1>
 	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos provident illum ullam eum cum soluta maxime fugit error tempore inventore, vero facilis. Consequuntur molestias cupiditate eveniet similique alias soluta perferendis.</p>
+	<br>
+	<h2>Let's try...</h2>
+	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos provident illum ullam eum cum soluta maxime fugit error tempore inventore, vero facilis. Consequuntur molestias cupiditate eveniet similique alias soluta perferendis.</p>
 </body>
 </html>
